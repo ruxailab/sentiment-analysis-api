55a2afcd777b779a536273b02484760eb18cb67b
Basma Elhoseny <basmaelhoseny6@gmail.com>
2025-01-15 23:51:46 +0200

    Removed Old Version To be Updated

--- app/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask_restx import Api
-
-# Routes
-from app.routes.ping_routes import api as ping_api
-
-def register_routes(api: Api):
-    api.add_namespace(ping_api, path='/ping')
-
-    return 
